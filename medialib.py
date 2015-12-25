000e17d7555e237c89ca844d1e899c1bba56f122
Dániel Kovács <daniel@kovacs.xyz>
2015-12-25 14:09:30 +0100

    refactoring

--- medialib.py
+++ medialib.py
@@ -12,37 +12,15 @@ import mutagen
 # unicode magic
 sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
 
-def check_naming(directory):
-    udir = unicode(directory, 'utf-8')
-    print 'Checking naming conventions recursively in directory', udir
-    for subdir, dirs, files in os.walk(udir):
-        if subdir != directory:
-            if subdir.find('Multimedia') is not -1:
-                continue
-            print '\tChecking directory', subdir,
-            if not files or not dirs or 'Multimedia' in dirs:
-                result = check_dir_naming(subdir)
-                if result != None:
-                    print
-                    print result
-                else:
-                    print '<= OK'
-            for f in files:
-                print '\t\tChecking file', f,
-                result = check_file_naming(f, subdir)
-                if result != None:
-                    print
-                    print result
-                else:
-                    print '<= OK'
-
+### GLOBALS ###
+# Ignore words for capitalization checks
 ignore_upper = [
   # en
   u'a', u'an', u'and', u'at',
   u'by',
   u'feat.', u'for', u'from',
   u'in', u'into',
-  u'of', u'on', u'or',
+  u'of', u'on', u'or'
   u'than', u'the', u'to',
   u'with',
   # it, es, etc.
@@ -59,140 +37,226 @@ ignore_upper = [
   u'és',
   u'vagy',
 ]
-name_regex = re.compile(u'.*')
-correct_spacing_regex = re.compile(u'( ,)|( .)|[ ]{2}')
+# Regular expressions
+name_regex = re.compile(u'.*') #TODO: something more clever?
+correct_spacing_regex = \
+  re.compile(u'( ,)|( .)|[ ]{2}') # No double-spaces
+                                  # No space before ',' or '.'
 
-cd_regex = re.compile(u'CD[1-9]')
-artist_regex = re.compile(u'(\w|[áéíóúäöüÁÉÍÓÚÄÖÜőűŐŰ])+')
+cd_regex = re.compile(u'CD[1-9]') # CD1, CD2, ..., CD9
+artist_regex = \
+  re.compile(u'(\w|[áéíóúäöüÁÉÍÓÚÄÖÜőűŐŰ])+') # Maybe additional characters
+                                              # would be needed
 type_regex = re.compile(
   u'((E|L|S)P)|(Compilation)|(Live)|(Remix)|(OST)|(Unofficial)'
-)
-year_regex = re.compile(u'0000|19[0-9]{2}|20[0-9]{2}')
+) # EP, LP, SP, ...
+year_regex = \
+  re.compile(u'0000|19[0-9]{2}|20[0-9]{2}') # 0000 == unknown
+                                            # 19xx-20xx
+
+music_extension_regex = re.compile(u'(mp3)|(flac)')
+aux_extension_regex = re.compile(u'(m3u)|(cue)|(log)')
+numbering_regex = \
+  re.compile(u'([1-9]\.)?[0-9]{2}') # stricly two digit (i.e., 01, 02, ..., 99)
+                                    # possibility to note CDs with n.01, etc.
+
+# Delimiters
+delim = {
+  'dir_path':u'/',
+  'parts':u' - ',
+  'ext':u'.',
+  'words':u' ',
+}
+
+# Messages
+msg = {
+  'check':u'Checking naming conventions recursively in directory',
+  'check_dir':u'\tChecking directory',
+  'check_file':u'\t\tChecking file',
+  'ok':u'<= OK',
+  'w_dir':u'W Incorrectly named directory ',
+  'w_dir_parts':u' <= Not correct number of parts',
+  'w_dir_artist':u' <= Incorrect artist naming',
+  'w_dir_second':u' <= Second part is not type nor year',
+  'w_dir_no_year_after_type':u' <= No year after type',
+  'w_dir_name':u' <= Incorrect album naming',
+  'w_spacing':u' <= Incorrect spacing',
+  'w_caps_start':u' <= Incorrect capitalization' + \
+    ' (must start with uppercase/number)',
+  'w_caps':u' <= Incorrect capitalization',
+  'w_&':u' <= Standalone character must be "a", "&", "I" or digit',
+  'a_caps':u'A Double-check capitalization for ',
+  'w_file_ext':u'W Incorrect file extension ',
+  'ff':u' for file ',
+  'w_file_numbering':u'W Incorrect numbering ',
+  'w_file_artist':u'W Incorrect artist name ',
+  'a_file_artist':u'A Double-check artist name ',
+  'w_file_name':u'W Incorrect song name ',
+  'w_file_ends_dot':u'A Song name ends with "."',
+  'w_file_spacing':u'W Incorrect spacing for file ',
+}
+
+### FUNCTIONS ###
+
+
+def check_naming(directory):
+    udir = unicode(directory, 'utf-8')
+    print msg['check'], udir
+    for subdir, dirs, files in os.walk(udir):
+        if subdir != directory:
+            if subdir.find('Multimedia') is not -1: # skipping Multimedia dirs
+                continue
+            print msg['check_dir'], subdir,
+            if not files or not dirs or 'Multimedia' in dirs:
+                # this conditions seems OK this far, revisit if need be
+                result = check_dir_naming(subdir)
+                if result != None:
+                    print
+                    print result
+                else:
+                    print msg['ok']
+            for f in files:
+                print msg['check_file'], f,
+                result = check_file_naming(f, subdir)
+                if result != None:
+                    print
+                    print result
+                else:
+                    print msg['ok']
+
+
+def check_capitalization(string, message, va = False):
+    parts = string.split(delim['words'])
+    if parts[0] == u'VA':
+        return None
+
+    start = 0
+    if not (parts[start][0].isupper() or parts[start][0].isdigit()):
+        return message + string + msg['w_caps_start']
+    start += 1
+
+    for i in range(start, len(parts)):
+        w = parts[i]
+        if len(w) == 1 and w != 'a' and w != '&' and w!= 'I' \
+          and not w.isdigit():
+            return message + string + msg['w_&']
+        if len(w) == 1: return
+
+        # Exactly one upper-case letter or all-number
+        if sum(1 for c in w if c.isupper()) != 1:
+            if w not in ignore_upper \
+              and (sum(1 for c in w if c.isdigit()) != len(w)):
+                return message + string + msg['w_caps'] + '/1'
+        
+        if not (w[0].isupper() or w[0].isdigit()):
+            if w[0] == '(':
+                if not w[1].isupper() and not w[1].isdigit() and w != '(feat.':
+                    return message + string + msg['w_caps'] + '/2'
+            elif w not in ignore_upper:
+                return message + string + msg['w_caps'] + '/3'
+            else:
+                return msg['a_caps'] + string + '/1'
+        if w[0].isupper() and w in ignore_upper:
+            return msg['a_caps'] + string + '/2'
+    return None
+
 
 def check_dir_naming(dir_name):
-    last_part = dir_name.split(u'/')[-1]
+    last_part = dir_name.split(delim['dir_path'])[-1]
 
     # It's a CD subdir
     if cd_regex.match(last_part) != None:
         return None
     
-    parts = last_part.split(u' - ')
-    if len(parts) != 3 and len(parts) != 4:
-        return 'W Incorrectly named directory ' + last_part + ' <= Not correct number of parts'
+    # Checking spacing
+    if correct_spacing_regex.match(last_part) != None:
+        return msg['w_dir'] + last_part + msg['w_spacing']
 
-    if artist_regex.match(parts[0]) == None:
-        return 'W Incorrectly named directory ' + last_part + ' <= Incorrect artist naming'
+    # Checking parts according to:
+    #   {artist} [- {type}] - {year} - {album name} - {FLAC|MP3@{bitrate}}
+    parts = last_part.split(delim['parts'])
+    if len(parts) != 3 and len(parts) != 4:
+        return msg['w_dir'] + last_part + msg['w_dir_parts']
+
+    index = 0
+    if artist_regex.match(parts[index]) == None:
+        return msg['w_dir'] + last_part + msg['w_dir_artist']
+    index += 1
 
     parts_type_declared = None
-    if type_regex.match(parts[1]) != None:
+    if type_regex.match(parts[index]) != None:
         parts_type_declared = True
-    elif year_regex.match(parts[1]) != None:
+    elif year_regex.match(parts[index]) != None:
         parts_type_declared = False
     else:
-        return 'W Incorrectly named directory ' + last_part + ' <= Second part is not type nor year'
-
-    if parts_type_declared:
-        if year_regex.match(parts[2]) == None:
-            return 'W Incorrectly named directory ' + last_part + ' <= No year after type'
-        if name_regex.match(parts[3]) == None:
-            return 'W Incorrectly named directory ' + last_part + ' <= Incorrect album naming (with type)'
-    else:
-        if name_regex.match(parts[2]) == None:
-            return 'W Incorrectly named directory ' + last_part + ' <= Incorrect album naming'
+        return msg['w_dir'] + last_part + msg['w_dir_second']
+    index += 1
     
-    artist_name = parts[0].split(u' ')
-    if artist_name[0] != 'VA':
-        if not artist_name[0][0].isupper() and not artist_name[0].isdigit():
-            return 'W Incorrectly named directory ' + last_part + ' <= Incorrect capitalization for artist (must start with capital/number)'
-        for s in artist_name:
-            if not s[0].isupper() and not s[0].isdigit():
-                if s not in ignore_upper:
-                    return 'W Incorrectly named directory ' + last_part + ' <= Incorrect capitalization for artist'
-                else:
-                    return 'A Double-check capitalization for artist ' + last_part
-    if correct_spacing_regex.match(parts[0]) != None:
-        return 'W Incorrect spacing in artist name ' + last_part
+    if parts_type_declared:
+        if year_regex.match(parts[index]) == None:
+            return msg['w_dir'] + last_part + msg['w_dir_no_year_after_type']
+        index += 1
 
-    album_name = (parts[3] if parts_type_declared else parts[2]).split(u' ')
-    if not album_name[0][0].isupper() and not album_name[0].isdigit():
-        return 'W Incorrectly named directory ' + last_part + ' <= Incorrect capitalization for album (must start with capital/number)'
-    for s in album_name:
-        if not s[0].isupper() and not s[0].isdigit() and s[0] != '&':
-            if s[0] == '(':
-                if not s[1].isupper() and not s[1].isdigit():
-                    return 'W Incorrectly named directory ' + last_part + ' <= Incorrect capitalization for album (word does not start uppercase/number)'
-                elif len(s) > 2 and s[2].isupper():
-                    return 'W Incorrectly named directory ' + last_part + ' <= Incorrect capitalization for album (word starts with double-capital)'
-            elif s not in ignore_upper:
-                return 'W Incorrectly named directory ' + last_part + ' <= Incorrect capitalization for album - word does not start uppercase/number'
-            elif len(s) > 1 and s[1].isupper():
-                return 'W Incorrectly named directory ' + last_part + ' <= Incorrect capitalization for album - word starts with double-capital'
-            else:
-                return 'A Double-check capitalization for album ' + last_part
-            if s[0].isupper() and s in ignore_upper:
-                return 'A Double-check capitalization for album ' + last_part
-    if correct_spacing_regex.match(
-      (parts[3] if parts_type_declared else parts[2])
-    ) != None:
-        return 'W Incorrect spacing in album name ' + last_part
+    if name_regex.match(parts[index]) == None:
+        return msg['w_dir'] + last_part + msg['w_dir_name']
+    
+    result = check_capitalization(parts[0], msg['w_dir'], va = True)
+    if result != None:
+        return result
+
+    result = check_capitalization(parts[index], msg['w_dir'], va = True)
+    if result != None:
+        return result
 
     #TODO: check for FLAC/MP3@ tag
 
     return None
 
-music_extension_regex = re.compile(u'(mp3)|(flac)')
-aux_extension_regex = re.compile(u'(m3u)|(cue)|(log)')
-numbering_regex = re.compile(u'([1-9]\.)?[0-9]{2}')
 
 def check_file_naming(file_name, dir_name):
-    extension = file_name.split(u'.')[-1]
+    extension = file_name.split(delim['ext'])[-1]
+
+    # Checking spacing
+    if correct_spacing_regex.match(file_name) != None:
+        return msg['w_file_spacing'] + file_name
+
     is_music = True
     if music_extension_regex.match(extension) == None:
         is_music = False
         if aux_extension_regex.match(extension) == None:
-            return 'W Incorrect file extension ' + extension + ' for file ' + file_name
+            return msg['w_file_ext'] + extension + msg['ff'] + file_name
 
     if is_music:
-        parts = file_name.split(u' - ', 3)
-        parts[2] = parts[2].rsplit(u'\.', 1)[0]
-        if numbering_regex.match(parts[0]) == None:
-            return 'W Incorrect numbering ' + parts[0] + ' for file ' + file_name
+        # Getting file name without extension
+        parts = file_name.split(delim['parts'], 3)
+        parts[2] = parts[2].rsplit(delim['ext'], 1)[0]
 
-        artist_name = dir_name.split(u'/')[-1]
+        if parts[2].endswith(u'.'):
+            return msg['w_file_ends_dot'] + parts[2] + msg['ff'] + file_name
+
+        # Checking file name according to:
+        #  {title number} - {artist name} - {song title}
+        if numbering_regex.match(parts[0]) == None:
+            return msg['w_file_numbering'] + parts[0] + msg['ff'] + file_name
+
+        # Get artist's name from dir
+        artist_name = dir_name.split(delim['dir_path'])[-1]
         if cd_regex.match(artist_name) != None:
-            artist_name = dir_name.split(u'/')[-2]
-        artist_name = artist_name.split(u' - ')[0]
+            artist_name = dir_name.split(delim['dir_path'])[-2]
+        artist_name = artist_name.split(delim['parts'])[0]
+
         if parts[1] != artist_name:
             if artist_name != u'VA':
-                return 'W Incorrect artist name ' + parts[1] + ' for file ' + file_name
+                return msg['w_file_artist'] + parts[1] + msg['ff'] + file_name
             else:
-                return 'A Double-check artist name ' + parts[1] + ' for file ' + file_name
+                return msg['a_file_artist'] + parts[1] + msg['ff'] + file_name
+
         if name_regex.match(parts[2]) == None:
-            return 'W Incorrect song name ' + parts[2] + ' for file ' + file_name
+            return msg['w_file_name'] + parts[2] + msg['ff'] + file_name
 
-        song_name = parts[2].split(u' ')
-        if not song_name[0][0].isupper() and not song_name[0].isdigit():
-            return 'W Incorrectly named song ' + parts[2] + ' <= Incorrect capitalization (must start with capital/number)'
-        for s in song_name:
-            if not s[0].isupper() and not s[0].isdigit() and s[0] != '&':
-                if s[0] == '(':
-                    if not s[1].isupper() and not s[1].isdigit():
-                        return 'W Incorrectly named song ' + parts[2] + ' <= Incorrect capitalization (word does not start with capital/number)'
-                    elif len(s) > 2 and s[2].isupper():
-                        return 'W Incorrectly named song ' + parts[2] + ' <= Incorrect capitalization (word starts with double-capital)'
-                elif s not in ignore_upper:
-                    return 'W Incorrectly named song ' + parts[2] + ' <= Incorrect capitalization - word does not start with capital/number'
-                elif len(s) > 1 and s[1].isupper():
-                    return 'W Incorrectly named song ' + parts[2] + ' <= Incorrect capitalization - word starts with double-capital'
-                else:
-                    return 'A Double-check capitalization for song ' + parts[2]
-            if s[0].isupper() and s in ignore_upper:
-                return 'A Double-check capitalization for song ' + parts[2]
-        if correct_spacing_regex.match(parts[2]) != None:
-            return 'W Incorrect spacing in song name ' + parts[2]
-
-        if parts[2].endswith(u'\.'):
-            return 'A Song name ends with "."' + parts[2]
+        result = check_capitalization(parts[2], msg['w_file_name'])
+        if result != None:
+            return result
 
     else:
         #TODO: non-music file name checking
